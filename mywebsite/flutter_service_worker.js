22fea879b1a8aabb585390fec12691f322867987
Trevor Claridge <trevclaridge@gmail.com>
2021-10-29 20:55:28 -0700

    turned it purple

--- mywebsite/flutter_service_worker.js
+++ mywebsite/flutter_service_worker.js
@@ -6,18 +6,22 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "e0217d9aa174cb259a804b5c78bc3964",
+"assets/NOTICES": "a896c6b106187889ac9979d9afeb4178",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
+"canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498",
+"canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
+"canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ae3a8725e54374c30c703c87d0656e3c",
-"/": "ae3a8725e54374c30c703c87d0656e3c",
-"main.dart.js": "b27d9f71faa5b89d7dd50b915d79b891",
+"index.html": "61544db8237905df3ab60ca5856e121f",
+"/": "61544db8237905df3ab60ca5856e121f",
+"main.dart.js": "212e222f5e77359574ee8b015c028d12",
 "manifest.json": "4734db19b089f1a4d7b37fdc9824bcd2",
-"version.json": "1f308f6516a18ecd7674c9a450a742cd"
+"version.json": "b3b87f9153d4406c14bc11865bbe1089"
 };
 
 // The application shell files that are downloaded before a service worker can
